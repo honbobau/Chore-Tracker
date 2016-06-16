9c1ebb34a0e8118ae84521a8cbbf9d8d61dc16b9
Spencer <spencer.toole@gmail.com>
2016-06-15 20:17:01 -0400

    more actions and comments

--- app/actions/chore.rb
+++ app/actions/chore.rb
@@ -1,7 +1,9 @@
+#brings up the page where admin can add or remove chores
 get "/change_list" do
 	erb :'/main/change_list'
 end
 
+#the action that creates a chore
 post "/create_chore" do
 	name = params[:name]
 	desc = params[:description]
@@ -10,18 +12,21 @@ post "/create_chore" do
   redirect "/main"
 end
 
+#attaches a user id to a chore
 put "/add_id/:id" do
 	chore = Chore.find(params[:id])
 	chore.update(user_id: current_user.id)
 	redirect '/main'
 end
 
+#allows a user to complete a task
 put '/complete/:id' do
 	chore = Chore.find(params[:id])
 	chore.update(completed: true)
 	redirect '/main'
 end
 
+#allows an admin to delete a chore
 delete '/removechore' do
 	chore = Chore.find(params[:chore_id])
 	chore.destroy
