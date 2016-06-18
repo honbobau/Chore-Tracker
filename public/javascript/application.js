e8145e25a3e2d1ba9eca59f400e268a578522343
vagrant <vagrant@lighthouse-labs>
2016-06-18 01:38:15 +0000

    front end

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
   $('.dropdown-toggle').dropdown();
+
+  // $("#sign-up-hover").hover(function(){
+  //   text = $(this).text();
+  //   $(this).text("Take me now!");
+  // },
+  // function() {
+  //   $(this).text(text);
+  // });
+
 });
